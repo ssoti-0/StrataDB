618b66d32141a865aac35a03c02aec3cef4de645
ssoti-0 <ssotirov40@gmail.com>
2026-02-20 19:47:40 +0200

    wip: Test for DiskManager (crashes with out of range)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include "storage/disk_manager.h"
+
+#include <cstring>
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout <<"StrataDB build OK\n";
+
+    const std::string test_file = "test.db";
+    std::remove(test_file.c_str());
+
+    stratadb::DiskManager dm(test_file);
+    std::cout <<"Created DiskManager, pages: " << dm.num_pages() << "\n";
+
+    stratadb::Page buf{};
+    const char* msg = "Hello StrataDB";
+    std::memcpy(buf.data(), msg, std::strlen(msg));
+    dm.write_page(0, buf);
+
+    stratadb::Page read_buf{};
+    dm.read_page(0, read_buf);
+    std::cout << "Read back: " << read_buf.data() << "\n";
+
+    std::remove(test_file.c_str());
     return 0;
 }
