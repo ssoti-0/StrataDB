616ac8e01ed6157c43760e5fd536bc3050e1c50d
ssoti-0 <ssotirov40@gmail.com>
2026-02-22 11:52:08 +0200

    wip: Node header file

---
+++ include/index/node.h
@@ -0,0 +1,14 @@
+#ifndef STRATADB_NODE_H
+#define STRATADB_NODE_H
+
+#include "storage/disk_manager.h"
+
+#include <array>
+#include <cstdint>
+#include <memory>
+
+namespace stratadb {
+
+}
+
+#endif
