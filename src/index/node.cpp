c1f00f44b827779e27b7d4ff47848bdd4c19a45f
ssoti-0 <ssotirov40@gmail.com>
2026-02-24 16:04:36 +0200

    wip: Added Leaf Node serialization

--- src/index/node.cpp
+++ src/index/node.cpp
@@ -18,23 +18,41 @@ namespace stratadb {
     }
 
     void LeafNode::serialize(Page& page) const {
+        
+        std::size_t offset = 0;
+        uint32_t is_leaf_raw = 1;
+        std::memcpy(page.data() + offset, &is_leaf_raw, sizeof(uint32_t));
+        offset += sizeof(uint32_t);
 
+        std::memcpy(page.data() + offset, &num_keys_, sizeof(uint32_t));
+        offset += sizeof(uint32_t);
+
+        std::memcpy(page.data() + offset, keys_.data(), ORDER * sizeof(int32_t));
+        offset += ORDER * sizeof(int32_t);
+
+        std::memcpy(page.data() + offset, values_.data(), ORDER * sizeof(int32_t));
+        offset += ORDER * sizeof(int32_t);
+
+        std::memcpy(page.data() + offset, &next_leaf_, sizeof(uint32_t));
     }
 
     std::unique_ptr<LeafNode> LeafNode::deserialize_leaf(const Page& page) {
         return std::make_unique<LeafNode>();
-
+         //To do
     }
 
     int LeafNode::find_key(int32_t key) const {
+        //To do
         return -1;
     }
 
     void InternalNode::serialize(Page& page) const {
+         //To do
 
     }
 
     std::unique_ptr<InternalNode> InternalNode::deserialize_internal(const Page& page) {
         return std::make_unique<InternalNode>();
+         //To do
     }
 }
